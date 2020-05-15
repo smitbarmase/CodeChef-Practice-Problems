891af86bb0109dc7557059bc217e653bc3f24943
Smit Barmase <heysmitbarmase@gmail.com>
2020-05-15 10:21:40 +0530

    LAPIN and FLOW007

--- DSA Learning Series/Contest-1/FLOW007.cpp
+++ DSA Learning Series/Contest-1/FLOW007.cpp
@@ -1,23 +1,25 @@
 #include <bits/stdc++.h>
-#define pb push_back
-#define f(i, a, n) for(int i = a; i < n; i++)
+#define endl '\n'
+typedef long long ll;
+using namespace std;
 int gcd(int a, int b) { if (!b) return a; return gcd(b, a % b); }  
 int lcm(int a, int b) { return (a * b) / gcd(a, b); }
-using namespace std;
 
 int main() {
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
   int t;
   cin >> t;
   while (t--)
   {
-    int n;
+    int n, rem, rev = 0;
     cin >> n;
     while (n != 0)
     {
-      cout << n % 10;
+      rem = n % 10;
+      rev = rev * 10 + rem;
       n /= 10;
     }
-    cout << endl;
+    cout << rev << endl;
   }
   return 0;
 }
--- playground.cpp
+++ playground.cpp
@@ -1,19 +1,27 @@
 #include <bits/stdc++.h>
-#define pb push_back
-#define f(i, a, n) for(int i = a; i < n; i++)
+#define endl '\n'
+#define ll long long
+#define cyes cout << "YES" << endl
+#define cno cout << "NO" << endl
+#define f(i, a, c) for (int i = a; i < c; i++)
 int gcd(int a, int b) { if (!b) return a; return gcd(b, a % b); }  
 int lcm(int a, int b) { return (a * b) / gcd(a, b); }
 using namespace std;
 
+void solve_test_case()
+{
+  int n;
+  cin >> n;
+  return;
+}
+
 int main() {
-  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
   int t;
   cin >> t;
   while (t--)
   {
-    int n;
-    cin >> n;
-    // Code here.
+    solve_test_case();
   }
   return 0;
 }
