4649711aa65b2739354115279d5712fde9821d07
Smit Barmase <heysmitbarmase@gmail.com>
2020-05-19 23:00:51 +0530

    playground-update

--- playground.cpp
+++ playground.cpp
@@ -2,6 +2,7 @@
 #define endl '\n'
 #define cyes cout << "YES" << endl
 #define cno cout << "NO" << endl
+#define f(i, a, n) for(int i = a; i < n; i++)
 #define ll long long
 int gcd(int a, int b) { if (!b) return a; return gcd(b, a % b); }  
 int lcm(int a, int b) { return (a * b) / gcd(a, b); }
