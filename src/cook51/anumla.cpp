6cb4ab6562b5928901cdd9e1fda15922b975aef5
Smit Barmase <smitbarmase@outlook.com>
2021-06-09 23:34:26 +0530

    anumla

--- src/cook51/anumla.cpp
+++ src/cook51/anumla.cpp
@@ -9,10 +9,52 @@ using namespace std;
 #define inf 1e18
 #define endl "\n";
 
+#define N 16
+
+int arr[1 << N], sum[1 << N];
+int res[N];
+
 void solve()
 {
     int n;
     cin >> n;
+    int m = 1 << n;
+    f(i, m) cin >> arr[i];
+    sort(arr, arr + m);
+    multiset<int> s;
+    int pos = 0, spos = 0;
+    for (int i = 1; i < m; i++)
+    {
+        int expected = -1;
+        if (!s.empty())
+        {
+            expected = *s.begin();
+        }
+        if (arr[i] != expected)
+        {
+            // new element found
+            res[pos] = arr[i];
+            int temp = spos;
+            for (int j = 0; j < temp; j++)
+            {
+                sum[spos] = sum[j] + res[pos];
+                s.insert(sum[spos]);
+                spos++;
+            }
+            sum[spos++] = res[pos];
+            pos++;
+        }
+        else
+        {
+            // it was expected sum
+            s.erase(s.begin());
+        }
+    }
+    for (int i = 0; i < pos; i++)
+    {
+        cout << res[i] << " ";
+    }
+    cout << endl;
 }
 
 int32_t main()
