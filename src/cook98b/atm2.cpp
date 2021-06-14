9cf565edb9e5117ea4c76a32ae1342ef051f0193
Smit <smitbarmase@outlook.com>
2021-06-14 19:05:05 +0530

    98

---
+++ src/cook98b/atm2.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define f(i, n) for (int i = 0; i < n; i++)
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n";
+
+void solve()
+{
+    int n, k, x;
+    cin >> n >> k;
+    string s = "";
+    f(i, n)
+    {
+        cin >> x;
+        if (x <= k)
+        {
+            k -= x;
+            s += "1";
+        }
+        else
+        {
+            s += "0";
+        }
+    }
+    cout << s << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
