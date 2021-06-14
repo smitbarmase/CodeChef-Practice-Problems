14e99a09d4b176b53eaf201cbf35ded3aba1f588
Smit <smitbarmase@outlook.com>
2021-06-14 14:50:12 +0530

    87

---
+++ src/cook87/ck87medi.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define f(i, n) for (int i = 0; i < n; i++)
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n";
+
+int a[1001];
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    f(i, n) cin >> a[i];
+    sort(a, a + n);
+    int idx = (n + k) / 2;
+    cout << a[idx] << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
