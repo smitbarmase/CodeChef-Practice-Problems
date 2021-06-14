b2cda47886475b2e80fcb3454b93019a89f50167
Smit <smitbarmase@outlook.com>
2021-06-14 20:35:08 +0530

    102b, 103b

---
+++ src/cook102b/adascool.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define f(i, n) for (int i = 0; i < n; i++)
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n";
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    if ((n * m) % 2 == 0)
+    {
+        cout << "YES" << endl;
+        return;
+    }
+    cout << "NO" << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ src/cook103b/tablet.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define f(i, n) for (int i = 0; i < n; i++)
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n";
+
+void solve()
+{
+    int n, b, w, h, p;
+    cin >> n >> b;
+    int a = -1;
+    f(i, n)
+    {
+        cin >> w >> h >> p;
+        if (p <= b && a < w * h)
+        {
+            a = w * h;
+        }
+    }
+    if (a == -1)
+    {
+        cout << "no tablet" << endl;
+        return;
+    }
+    cout << a << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
