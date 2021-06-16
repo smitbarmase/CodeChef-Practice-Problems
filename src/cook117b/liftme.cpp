7be4e135d331cb79e7ed8c1ba67c339a90f874a7
Smit <smitbarmase@outlook.com>
2021-06-16 14:42:51 +0530

    117

---
+++ src/cook117b/liftme.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define f(i, n) for (int i = 0; i < n; i++)
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+
+void solve()
+{
+    int n, q;
+    cin >> n >> q;
+    int count = 0;
+    int i = 0;
+    while (q--)
+    {
+        int a, b;
+        cin >> a >> b;
+        count += abs(a - i) + abs(a - b);
+        i = b;
+    }
+    cout << count << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
