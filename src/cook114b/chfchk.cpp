70e8eb30c88f9e9dd778c455eeaf1d280ab2eae2
Smit <smitbarmase@outlook.com>
2021-06-16 14:42:29 +0530

    114

---
+++ src/cook114b/chfchk.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define f(i, n) for (int i = 0; i < n; i++)
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+
+int a[100];
+
+void solve()
+{
+    int n;
+    cin >> n;
+    f(i, n) cin >> a[i];
+    cout << *min_element(a, a + n) << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
