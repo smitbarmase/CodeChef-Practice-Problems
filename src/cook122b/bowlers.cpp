a0740b37bc481e4eda47ba84f80a72a1fc9792fc
Smit <smitbarmase@outlook.com>
2021-06-16 14:45:00 +0530

    bowlers

---
+++ src/cook122b/bowlers.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define endl '\n'
+
+int gcd(int a, int b)
+{
+    if (!b)
+        return a;
+    return gcd(b, a % b);
+}
+int lcm(int a, int b) { return (a * b) / gcd(a, b); }
+
+int a[10000];
+
+void solve()
+{
+    int n, k, l;
+    cin >> n >> k >> l;
+    if (n > k * l)
+    {
+        cout << -1 << endl;
+        return;
+    }
+    if (k == 1 && n > 1)
+    {
+        cout << -1 << endl;
+        return;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << (i % k) + 1 << " ";
+    }
+    cout << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
